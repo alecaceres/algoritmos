07aa527dc61e72c120b01cd4f3c78d0694321597
alecaceres <aleda997@gmail.com>
2020-03-08 15:35:37 -0300

    Se agrego la propiedad nodos a la clase grafo

--- reinas.py
+++ reinas.py
@@ -4,9 +4,14 @@ class nodo:
         self.columna = columna
         self.vecinos = []
 
+    def addVecino(self, vecinos):
+        for vecino in vecinos:
+            self.vecinos.append(vecino)
+
 class grafo:
     def __init__(self, n):
-        nodo(0,0) # el tablero vacío
+        self.nodos = []
+        self.nodos.append(nodo(0,0)) # el tablero vacío
         for i in range(1,n+1):
             for j in range(1, n+1):
-                nodo(i, j)
+                self.nodos.append(nodo(i, j))
