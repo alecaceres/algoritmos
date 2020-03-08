31883d8e87cd5c5f34ba236d71401fa81ce8280c
alecaceres <aleda997@gmail.com>
2020-03-08 15:49:07 -0300

    Se cambian listas por sets
    De la clase nodo, self.vecinos pasa de ser una lista a ser un set. En la
    clase grafo pasa lo mismo con self.nodos

--- reinas.py
+++ reinas.py
@@ -2,16 +2,18 @@ class nodo:
     def __init__(self, fila, columna):
         self.fila = fila
         self.columna = columna
-        self.vecinos = []
+        self.vecinos = set()
 
     def addVecino(self, vecinos):
         for vecino in vecinos:
-            self.vecinos.append(vecino)
+            self.vecinos = self.vecinos.add(vecino)
 
 class grafo:
     def __init__(self, n):
-        self.nodos = []
-        self.nodos.append(nodo(0,0)) # el tablero vacío
+        self.nodos = set()
+        self.nodos = self.nodos.add(nodo(0,0)) # el tablero vacío
         for i in range(1,n+1):
             for j in range(1, n+1):
-                self.nodos.append(nodo(i, j))
+                self.nodos = self.nodos.add(nodo(i, j))
+        for v in self.nodos:
+            enlazar(v, self.nodos)
