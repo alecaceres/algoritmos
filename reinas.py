2748b9473fa7841c384789904b39c12185cb523c
alecaceres <aleda997@gmail.com>
2020-03-08 16:36:56 -0300

    Construccion del tablero terminada

--- reinas.py
+++ reinas.py
@@ -6,12 +6,12 @@ class nodo:
 
     def addVecino(self, vecinos):
         for vecino in vecinos:
-            self.vecinos = self.vecinos.add(vecino)
+            self.vecinos.add(vecino)
 
 class grafo:
     def __init__(self, n):
         self.filas = set()
-        self.filas = self.filas.add(tuple([nodo(0,0)])) # el tablero vacío
+        self.filas.add(tuple([nodo(0,0)])) # el tablero vacío
         for i in range(1,n+1):
             fila = []
             for j in range(1, n+1):
@@ -21,13 +21,13 @@ class grafo:
 
     def recorrerFilas(tablero, n):
         for fila in tablero.filas:
-            if fila[0].fila =! n:
+            if fila[0].fila != n:
                 filaVecino = fila[0].fila + 1
                 for fila2 in tablero.filas:
                     if fila2[0].fila == filaVecino:
                         vecinos = set()
                         for v in fila2:
-                            vecinos = vecinos.add(v)
+                            vecinos.add(v)
                 for v in fila:
                     v.addVecino(vecinos)
 
