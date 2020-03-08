f59a563ea206909f3eb6a9487a4e0e9b30934fb1
alecaceres <aleda997@gmail.com>
2020-03-08 15:57:02 -0300

    Los nodos son representados como set de tuplas

--- reinas.py
+++ reinas.py
@@ -11,9 +11,11 @@ class nodo:
 class grafo:
     def __init__(self, n):
         self.nodos = set()
-        self.nodos = self.nodos.add(nodo(0,0)) # el tablero vacío
+        self.nodos = self.nodos.add(tuple([nodo(0,0)])) # el tablero vacío
         for i in range(1,n+1):
+            fila = []
             for j in range(1, n+1):
-                self.nodos = self.nodos.add(nodo(i, j))
+                fila.append(nodo(i, j))
+            self.nodos.add(tuple(fila))
         for v in self.nodos:
             enlazar(v, self.nodos)
