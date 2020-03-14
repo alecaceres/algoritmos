be311d445da21736b85de09b02504064956909c5
alecaceres <aleda997@gmail.com>
2020-03-13 23:27:56 -0300

    Ya se genera el tablero

--- caballo.py
+++ caballo.py
@@ -33,41 +33,20 @@ class grafo:
         self.nodos = set()
         self.soluciones = 0
 
-        self.addNodos(n)
-
-        for node in self.addNodos(n):
-            self.nodos.add(node)
-            if len(self.nodos) == n**2: break
-        #for i in range(1,n+1):
-        #    for j in range(1, n+1):
-        #        self.agregar(i, j)
-        #        self.nodos.add(nodo(i, j))
-                #y tiene que agregar tambien los vecinos, y esos vecinos su vecinos y así ir verificando también que no se agregen lo mismo
-                #dos veces. Siempre que se cree un vecino, asignar también como vecino al nodo anterior
+        #self.addNodos(n)
+        for i in range(1,n+1):
+            for j in range(1, n+1):
+                self.nodos.add(self.addNodos(n, nodo(i,j)))
 
     def addNodos(self, dim = 8, celda = nodo(1,1)):
         i = celda.fila
         j = celda.columna
-        print(i, j)
         vecinos = {tuple([i-1, j-2]), tuple([i-1, j+2]), tuple([i+1, j-2]), tuple([i+1, j+2]),
                 tuple([i-2, j-1]), tuple([i-2, j+1]), tuple([i+2, j-1]), tuple([i+2, j+1])}
         vecinos = limpiar(vecinos, dim) # eliminar las celdas inválidas
-        print(vecinos)
         for i,j in vecinos:
-            existe = False
-            for nodito in self.nodos:
-                if nodito.fila == i and nodito.columna == j:
-                    existe = True
-                    break
-            if not existe:
-                nodito = nodo(i,j)
-            nodito.vecinos.add(celda)
-            celda.vecinos.add(nodito)
-        #self.nodos.add(celda)
-        for vecino in celda.vecinos:
-            yield self.addNodos(dim, vecino)
-            print("Tamaño:",len(self.nodos),"\n")
-        yield celda
+            celda.vecinos.add(nodo(i,j))
+        return celda
 
 def limpiar(lista, dim):
     lista2 = lista.copy()
@@ -80,6 +59,5 @@ def noesValido(tupla, dim):
     i, j = tupla
     return i<1 or j<1 or i>dim or j>dim
 
-a = grafo(3)
+a = grafo()
 print(a)
-print(len(a.nodos))
