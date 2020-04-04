b8d82151c00ff61ef64f139f8c8120bda6f03705
alecaceres <aleda997@gmail.com>
2020-04-04 12:16:46 -0400

    Ya se hace bien. Queda eliminar los ceros

--- station_balance.py
+++ station_balance.py
@@ -1,4 +1,6 @@
 from statistics import mean
+import sys
+
 def imbalance(C, S, masses):
     masses.sort()
     A = sum(masses)/C
@@ -6,13 +8,18 @@ def imbalance(C, S, masses):
     masses = padding + masses
     imbalance = 0
     for i in range(C):
-        print("Contenedor", i, "\t", masses[i], masses[2*C-i-1])
+        print("Contenedor", str(i) + ":", masses[i], masses[2*C-i-1])
         Xi = (masses[i] + masses[2*C-i-1])
         imbalance += abs(Xi-A)
     print("Imbalace=", imbalance, '\n')
 
-
-C = 2
-S = 3
-masses = [6, 3, 8]
-imbalance(C, S, masses)
+set_number = 1
+for i, line in enumerate(sys.stdin):
+    linea = line.rstrip() # se eliminan los saltos de línea de creados al leer las líneas de sys.stdin
+    if i%2 == 0:
+        C, S = [int(x) for x in linea.split()]
+    else:
+        masses = [int(x) for x in linea.split()]
+        print("Set #"+str(set_number))
+        imbalance(C, S, masses)
+        set_number += 1
