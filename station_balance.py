e7d44f0de7746caee01c7cc39d5013097c0a1e74
alecaceres <aleda997@gmail.com>
2020-04-04 12:08:50 -0400

    Se terminó imbalance. Falta lectura de stdinput

--- station_balance.py
+++ station_balance.py
@@ -1,9 +1,15 @@
+from statistics import mean
 def imbalance(C, S, masses):
     masses.sort()
+    A = sum(masses)/C
     padding = [0]*(2*C-S)
     masses = padding + masses
-    print(masses)
-
+    imbalance = 0
+    for i in range(C):
+        print("Contenedor", i, "\t", masses[i], masses[2*C-i-1])
+        Xi = (masses[i] + masses[2*C-i-1])
+        imbalance += abs(Xi-A)
+    print("Imbalace=", imbalance, '\n')
 
 
 C = 2
