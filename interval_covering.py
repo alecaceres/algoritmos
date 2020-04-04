c571437a9e78b92103b625f27352dc750586ef63
alecaceres <aleda997@gmail.com>
2020-04-04 15:44:34 -0400

    Resuelto interval covering

--- interval_covering.py
+++ interval_covering.py
@@ -1,6 +1,22 @@
+import sys
+
 def intervals(X):
+    print("X =", X)
     X.sort()
+    print("\nX ordenado:", X)
     S = recursion(X, [])
+    print("\nS=", S, "\n\n\n")
+
 
 def recursion(X,S):
-    
+    if X:
+        S.append((X[0], X[0]+1))
+        X = list(filter((X[0]+1).__le__, X))
+        return recursion(X, S)
+    return S
+
+for i, line in enumerate(sys.stdin):
+    print("------------------------------------\nTest", i+1, end = "\n\n")
+    linea = line.rstrip()
+    X = [float(x) for x in linea.split()]
+    intervals(X)
