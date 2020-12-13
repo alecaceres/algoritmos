7009a60473257117cde398b5de1b9d0d1ccc2a12
alecaceres <aleda997@gmail.com>
2020-12-13 00:25:18 -0300

    Check winner arreglado

--- inpHeXion/game_utils.py
+++ inpHeXion/game_utils.py
@@ -1,64 +1,108 @@
-def checkWinner(board):
-    players = (None, 'white', 'black')
-    winner = []
-    for player in {1, 2}: winner.append(board, player)
-    return winner
+import networkx as nx
+from networkx.algorithms.shortest_paths.generic import has_path
+import itertools
+import random
 
-def playerStatus(board, player):
-    to_check_u = [(0, pos) for pos,value in enumerate(board[0]) if value == player]
-    to_check_l = [(pos, 0) for pos,value in enumerate(list(zip(*board))[0]) if value == player]
-    for top in to_check_u:
-        if solveMaze(board, top, player, goal = (6, None)): return True
-        print('hola, bienvenido a player status')
-    for left in to_check_l:
-        if solveMaze(board, left, player, goal = (None, 6)): return True
-    return False # si no existe camino entre lados opuestos
+def addNode(G, node):
+    i,j = node
+    G.add_node(i+j*7)
+    return G
 
-def pathExists(board, pos, player, goal):
-    i,j = pos
-    if board[i][j] != player: return False
-    if i == goal[0] or j == goal[1]: return True
-    possible_movements = {(i+1,j),(i-1,j),(i,j+1),(i,j-1),(i+1,j-1),(i-1,j+1)}
-    if goal[1] is None:
-        possible_movements.remove((i-1, j))
-        possible_movements.remove((i-1, j+1))
-    elif goal[0] is None:
-        possible_movements.remove((i, j-1))
-        possible_movements.remove((i+1, j-1))
+def addFirstPlayer(G, dest, player):
+    print("Adding first player...")
+    print("Destination:", dest)
+    i,j = dest
+    print("Dest:", i,j)
+    G.nodes[i+j*7]['player'] = player
+    G = addEdges(G, *dest, player)
+    print("Edges:",G.edges)
+    return G, getBoard(G)
 
-def solveMaze(board, pos, player, goal):
-    print('empanada')
-    print(pos)
-    x_goal, y_goal = goal
-    x_pos, y_pos = pos
-    sol = [ [ 0 for j in range(7) ] for i in range(7) ]
+def addPlayer(G, src, dest, player):
+    '''
+    G:      Grafo del tablero
+    src:    Posición de la ficha del rival por mover
+    dest:   Posición a la cual la ficha del rival quiere moverse
+    player: El que realiza la jugada
+    '''
+    i_src, j_src = src # nodo de origen (donde se hace el primer clic)
+    i_dest, j_dest = dest # nodo de destino (donde se hace el segundo clic)
+    src_pos = i_src+j_src*7 # posición del origen en notación vectorial (no matricial)
+    dest_pos = i_dest+j_dest*7 # posición del destino en notación vectorial (no matricial)
+    G.nodes[src_pos]['player'] = player # el nodo del origen se queda pintado en el color del player
+    G.nodes[dest_pos]['player'] = 1 if player==2 else 2 # el nodo de destino se queda pintado en el color del rival
+    G.remove_edge(src_pos, dest_pos, key = 'possible_movement') # se elimina la arista src_dest como posible mov.
+    G = updatePlayers(G, src_pos, src, player)
+    print("Edges to remove:", [edge for edge in G.edges(dest_pos) if G.has_edge(*edge, key = 'possible_movement')])
+    G.remove_edges_from([edge for edge in G.edges(dest_pos) if G.has_edge(*edge, key = 'possible_movement')])
+    G = addEdges(G, *dest, player)
+    print("Player", player, "movió de la posición", src_pos, "a la", dest_pos)
+    return G, getBoard(G)
 
-    if solveMazeUtil(board, x_pos, y_pos, sol, x_goal, y_goal, player):
-        print("solution does exist")
-        return True
-    print("Solution doesn't exist");
-    return False
+def addEdges(G, i, j, player):
+    possible_movements = {(i,j+1),(i,j-1),(i+1,j),(i-1,j),(i-1,j+1),(i+1,j-1)}
+    possible_movements = [(i+j*7,
+                           movement[0] + movement[1]*7,
+                           G.nodes[movement[0]+movement[1]*7]['player']
+                           ) for movement in possible_movements if movement
+                           in [(i,j) for i in range(7) for j in range(7)]] # si la posición existe en el tablero
+    [G.add_edge(i,j, key = 'possible_movement') for (i,j,p) in possible_movements if not p]
+    [G.add_edge(i,j, key = 'path') for (i,j, p) in possible_movements if p and p!=player]
+    return G
+def updatePlayers(G, src_pos, src, player):
+    '''
+    Se actualizan las aristas (tienen la etiqueta de possible_movement si
+    puede mover del nodo fuente al nodo destino y la etiqueta de path si
+    forma un camino para uno de los jugadores).
+    '''
+    i,j = src
+    path_edges = {(i,j+1),(i,j-1),(i+1,j),(i-1,j),(i-1,j+1),(i+1,j-1)}
+    print("\nMoving from source", src, "=", i+j*7)
+    print("Current possible movements:")
+    for movement in path_edges:
+        if movement in [(i,j) for i in range(7) for j in range(7)]:
+            print(f"Player {G.nodes[movement[0]+movement[1]*7]['player']}\tMovement: {movement[0]+movement[1]*7}")
+    path_edges = [(i+j*7, movement[0] + movement[1]*7)
+                            for movement in path_edges
+                            if (movement in [(i,j) for i in range(7) for j in range(7)] # si la posición existe en el tablero
+                                and G.nodes[movement[0]+movement[1]*7]['player'] == player)]
+    print(f"\nUpdating...\nSource: {src}\tSource position: {src_pos}\tPlayer: {player}\nPossible movements: {path_edges}")
+    G.remove_edges_from([edge for edge in G.edges(src_pos) if not G.has_edge(*edge, key = 'possible_movement')])
+    [G.add_edge(i,j, key = 'path') for (i,j) in path_edges]
+    return G
 
-def isSafe(maze, x, y, player):
+def hasWon(G, player):
+    '''
+    Esta función devuelve un booleano para saber si el jugador ha
+    ganado la partida.
+    '''
+    G = G.copy()
+    G.remove_edges_from([edge for edge in G.edges if 'possible_movement' in edge])
+    all_possible_paths = (list(itertools.product(range(7), range(41,49))
+                        if player == 2 else list(itertools.product(range(0,49,7), range(6,49,7)))))
+    print("Edges with paths:", G.edges, "\nHas path:", [(player, src, dest, has_path(G, src, dest))
+                for (src, dest) in all_possible_paths
+                if (G.nodes[src]['player'] == player and G.nodes[dest]['player'] == player)])
+    return any([has_path(G, src, dest)
+                for (src, dest) in all_possible_paths
+                if (G.nodes[src]['player'] == player and G.nodes[dest]['player'] == player)])
 
-    if x >= 0 and x < 7 and y >= 0 and y < 7 and maze[x][y] == player:
-        return True
+def getPossibleMovements(G, player):
+    edges = [edge for edge in G.edges]
+    edges.extend([(y,x,attr) for (x,y,attr) in edges])
+    return [edge[1::-1] for edge in edges
+            if (G.nodes[edge[0]]['player'] == (1 if player==2 else 2)
+               and 'possible_movement' in edge)]
 
-    return False
+def nextBlackMovement(G):
+    '''
+    Esta función se encarga de implementar la lógica de la siguiente
+    jugada del jugador negro.
+    '''
+    (src, dest) = random.choice(getPossibleMovements(G, player = 2))
+    dest, src = (dest, src) if G.nodes[dest]['player'] != 1 else (src, dest)
+    input('Enter para continuar')
+    return (src%7, src//7), (dest%7, dest//7)
 
-def solveMazeUtil(maze, x, y, sol, x_goal, y_goal, player):
-    print("estoy en", x, y, "\t goal", x_goal, y_goal)
-    if x == x_goal or y == y_goal:
-        sol[x][y] = 1
-        print('ya es si que true')
-        return True
-    print('no es true')
-    if isSafe(maze, x, y, player) == True:
-        print("sol[x][y]", x, y)
-        sol[x][y] = 1
-        if solveMazeUtil(maze, x + 1, y, sol, x_goal, y_goal, player) == True:
-            return True
-        if solveMazeUtil(maze, x, y + 1, sol, x_goal, y_goal, player) == True:
-            return True
-        sol[x][y] = 0
-        return False
+def getBoard(G):
+    return [[G.nodes[i+j*7]['player'] for j in range(7)] for i in range(7)]
--- inpHeXion/main.py
+++ inpHeXion/main.py
@@ -1,52 +1,55 @@
 from inpHeXionWindow import start_window
-from black_player import next_movement
-from game_utils import playerStatus
+from game_utils import *
 import time
+import networkx as nx
 
 def main(window):
-    board = []
-    for i in range(7):
-        board.append([])
-        for j in range(7):
-            board[i].append(0)
-    board = [[0]*7 for i in range(7)]
+    '''
+    Player 1: Blanco
+    Player 2: Negro
+    Player 0: Default, sin jugador
+    '''
+    G = nx.MultiGraph()
+    G.add_nodes_from(range(49), player = 0)
+    board = getBoard(G)
 
     # imprimir tablero en la venta
     window.print_board(board)
 
     # la primera vez se puede colocar la ficha en cualquier parte
-    print('Haga click sobre la celda para imprimir el indice de la celda correspondiente.')
     pos = window.scan_position()
-    board[pos[0]][pos[1]] = 1
-    print('Click en la celda (' + str(pos[0]) + ', ' + str(pos[1]) + ')')
+    G, board = addFirstPlayer(G, pos, 1)
     window.print_board(board) # imprimir tablero en la venta
 
     while(True):
-        playerStatus(board, 2)
+        if hasWon(G, player = 1):
+            print("Ganó el jugador blanco")
+            time.sleep(60)
+            break
         # turno de la ficha negra
         time.sleep(1)
-        board = next_movement(board)
+        src, dest = nextBlackMovement(G)
+        G, board = addPlayer(G, src, dest, 2)
         window.print_board(board)
-        playerStatus(board, 1)
+        if hasWon(G, player = 2):
+            print("Ganó el jugador negro")
+            time.sleep(60)
+            break
         while True:
             pos = window.scan_position()
             if board[pos[0]][pos[1]] != 2: # solo se puede elegir una celda con ficha negra
                 print('Celda no válida')
             else:
-                print(f'Click en la celda ({pos[0]}, {pos[1]})')
-                print('Indique la celda a la cual la ficha negra debe moverse')
                 while True:
                     pos_move = window.scan_position()
-                    print(abs(pos[0]-pos_move[0]) + abs(pos[1]-pos_move[1]))
-                    i,j = pos
-                    possible_movements = {(i+1,j),(i-1,j),(i,j+1),(i,j-1),(i+1,j-1),(i-1,j+1)}
-                    possible_movements = [movement for movement in possible_movements
-                                        if not board[movement[0]][movement[1]]]
-                    if pos_move not in possible_movements:
+                    possible_movements = [(node%7, node//7) for edge in
+                                            getPossibleMovements(G, player = 1)
+                                            for node in edge
+                                            if pos[0]+pos[1]*7 in edge]
+                    if pos_move not in possible_movements or pos_move == pos:
                         print('Celda no válida')
                     else:
-                        board[pos_move[0]][pos_move[1]] = 2
-                        board[pos[0]][pos[1]] = 1
+                        G, board = addPlayer(G, pos, pos_move, 1)
                         window.print_board(board) # imprimir tablero en la venta
                         break
                 break # turno de la ficha negra
